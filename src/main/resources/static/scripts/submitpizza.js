4951870bd9f2eca3a7c04ccf873669c265e9c4d3
jtadmin <jtadmin@RED-CPU-117.redph.local>
2017-09-28 16:16:59 +0800

    commit ui changes

--- src/main/resources/static/scripts/submitpizza.js
+++ src/main/resources/static/scripts/submitpizza.js
@@ -1,4 +1,48 @@
 function submitPizza(){
-alert("button is clicked!");
-return true;
+	var address= {
+		street: $("#your_street").val(),
+		number: $("#your_street_number").val(),
+		complement: $("#your_complement").val(),
+		postalCode: $("#your_postal").val(),
+		city: $("#your_city").val(),
+		country: $("#your_country").val()
+	}
+	var payment = {
+		provider: $("#your_cc_provider").val(),
+		number: $("#your_cc_num").val(),
+		expiration: $("#your_cc_expire").val()
+	}
+	var customer = {
+		name: $("#your_name").val(),
+		email: $("#your_email").val(),
+		address: address,
+		payment: payment
+	}		
+	
+	var menuorder = {
+		restaurant: $("#your_restaurant").val(),
+		pizza: $("#pizzaChoice option:selected").attr("id")
+		
+		
+	}		
+	
+	var data = {
+		customer: customer,
+		menuOrder: menuorder
+	}
+	
+	$('#target').html('sending..');
+	
+	$.ajax({
+	            url: '/userinfo/PizzaSubmit',
+	            type: 'post',
+	            dataType: 'json',
+	            success: function (data) {
+	                $('#target').html(data.msg);
+	            },
+	            data: data
+	        });
+	
+	//alert(JSON.stringify(data));
 }
+
